24e6c8951040b691a1385354b18f810b2a6f09de
= <=>
2025-09-07 15:41:16 -0300

    salvando arquivos de calmaria spa 100% concluido

---
+++ 3489-calmaria-spa-javascript-main/main.js
@@ -0,0 +1,124 @@
+let ultimoElementoFocado;
+
+function gerenciarFocoModal(modalId){
+    const modal = document.querySelector(`#${modalId}`);
+    const elementosModal = modal.querySelectorAll('a, button, input, textarea, select, [tabindex]:not([tabindex="-1"])');
+
+    const primeiroElemento = elementosModal[0];
+    const ultimoElemento = elementosModal[elementosModal.length -1];
+    primeiroElemento.focus();
+
+    modal.addEventListener("keydown", (event)=>{
+        if(event.key === "Tab"){
+            if(event.shiftKey){
+                //se a tecla shift + tab for pressionada e o foco estiver no primeiro elemento, mover para o ultimo elemento
+                if(document.activeElement === primeiroElemento){
+                    event.preventDefault();
+                    ultimoElemento.focus();
+            }
+        }else{
+            //se a tecla tab for pressionada e o foco estiver no ultimo elemento, mover para o primeiro elemento
+            if(document.activeElement === ultimoElemento){
+                event.preventDefault();
+                primeiroElemento.focus();
+            }
+        }
+    }
+    })
+}
+
+
+function alternarModal(modalId, abrir){
+    const modal = document.querySelector(`#${modalId}`)
+
+    if(abrir){
+        ultimoElementoFocado = document.activeElement;
+        modal.style.display = 'block';
+        gerenciarFocoModal(modalId);
+    }else{
+        modal.style.display = 'none';
+        if(ultimoElementoFocado){
+            ultimoElementoFocado.focus();
+        }
+    }
+
+    document.body.style.overflow = abrir ? "hidden": "auto";
+}
+
+document.addEventListener("keydown", (event)=>{
+    if(event.key === 'Escape'){
+        alternarModal(`ver-modal-inscrito`, false);
+        alternarModal('ver-modal-contato', false);
+        alternarModal('ver-modal-enviado', false);
+
+        document.querySelectorAll(".cabecalho__lista-item").forEach(item => {
+            alternarSubmenu(item, false);
+        });
+    }
+})
+
+//Função pra alterar o submenu 
+function alternarSubmenu(item, mostrar){
+    const submenu = item.querySelector('.submenu');
+
+
+    if(submenu){
+        submenu.style.display = mostrar ? 'block' : 'none';
+
+        const menuItem = item.querySelector('.cabecalho__lista-item a');
+        menuItem.setAttribute("aria-expanded", mostrar ? true : false);
+
+        const DropdownExpandedIcon = item.querySelector('.material-symbols-outlined.icone');
+
+        DropdownExpandedIcon.classList.toggle('active', mostrar);
+    }
+}
+
+//selecionar todos os itens do menu
+document.querySelectorAll('.cabecalho__lista-item').forEach(item =>{
+    //adicionar um ouvinte mouseover
+    item.addEventListener("mouseover", ()=> alternarSubmenu(item, true));
+
+    //adicionar um ouvinte mouseout
+    item.addEventListener("mouseout", ()=> alternarSubmenu(item, false));
+
+    item.addEventListener("click", () => {
+        const submenu = item.querySelector('.submenu');
+
+        const isDisplayed = submenu.style.display ==="block";
+
+        alternarSubmenu(item, !isDisplayed);
+    })
+})
+
+
+//acordion
+
+document.querySelectorAll(".botao-acordeao"). forEach(button => {
+    button.addEventListener("click", ()=>AlternarAcordeao(button))
+});
+
+function AlternarAcordeao(button){
+    const isAlreadyOpen= button.getAttribute("aria-expanded") === "true";
+
+    document.querySelectorAll(".botao-acordeao").forEach((btn) =>{
+        btn.setAttribute("aria-expanded", "false")
+
+        const content = btn.nextElementSibling;
+        content.classList.remove("expandido");
+        content.setAttribute("aria-hidden", "true");
+    })
+    if(!isAlreadyOpen){
+        button.setAttribute("aria-expanded", "true");
+
+        const content = button.nextElementSibling;
+        content.classList.add("expandido");
+
+        content.setAttribute("aria-hidden", "false");
+    }
+}
+
+//limpa formulario
+function limparFormulario(){
+    document.querySelector("#form-contato").reset();
+}
